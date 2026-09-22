8482dd9ec5b7bdc892b5a53ceebd20b4109d00c6
Kartavya Agarwal <155150892+Digital-Excellencee@users.noreply.github.com>
2026-09-22 11:44:12 +0530

    Add Google Analytics tag

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7N9MJ9FB39"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7N9MJ9FB39');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
